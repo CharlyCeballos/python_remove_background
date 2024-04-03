8e10a141ba99776eaed2710e3fa47e8decfbd00d
CharlyCeballos <charlyfaceam912@gmail.com>
2024-04-03 13:12:59 -0600

    standardizing extension files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from rembg import remove
 from PIL import Image
 
-input_path = '01.png'
-output_path = 'output' + input_path
+input_path = '02.jpg'
+name_file = input_path.split('.')
+output_path = 'output' + name_file[0] + '.png'
+print(output_path)
 
 input = Image.open(input_path)
 output = remove(input)
